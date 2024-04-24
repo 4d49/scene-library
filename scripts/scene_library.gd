7b385da8d46c9c6cf672430b8e0c56a3d25d0a98
Mansur Isaev <737dab2f169a@mail.ru>
2024-04-24 11:12:51 +0400

    Fix multithreading
    At least, I really hope so...

--- scripts/scene_library.gd
+++ scripts/scene_library.gd
@@ -84,6 +84,8 @@ signal show_in_file_system_request(path: String)
 signal show_in_file_manager_request(path: String)
 signal asset_display_mode_changed(display_mode: DisplayMode)
 
+signal thumb_created(texture: Texture2D)
+
 
 enum CollectionTabMenu {
 	NEW,
@@ -980,13 +982,35 @@ func _save_thumb_to_disk(id: int, image: Image) -> void:
 	var error := image.save_png(_get_thumb_cache_path(ResourceUID.get_id_path(id)))
 	assert(error == OK, error_string(error))
 
+func _create_thumb() -> void:
+	await RenderingServer.frame_pre_draw
+	_viewport.set_update_mode(SubViewport.UPDATE_ONCE)
+
+	await RenderingServer.frame_post_draw
+
+	var texture: Texture2D = _viewport.get_texture()
+	thumb_created.emit(texture)
+
 @warning_ignore("return_value_discarded")
 func _thread_process() -> void:
 	var semaphore := Semaphore.new()
 
-	var preview_frame_started := func() -> void:
-		_viewport.set_update_mode(SubViewport.UPDATE_ONCE)
-		RenderingServer.request_frame_drawn_callback(semaphore.post)
+	var update_thumb := func(texture: Texture2D, item: Dictionary) -> void:
+		if is_instance_valid(texture):
+			var image: Image = texture.get_image()
+
+			image.resize(THUMB_GRID_SIZE, THUMB_GRID_SIZE, Image.INTERPOLATE_LANCZOS)
+			var thumb_large: ImageTexture = item["thumb"]["large"]
+			thumb_large.update(image)
+
+			if _cache_enabled:
+				_save_thumb_to_disk(item["id"], image)
+
+			image.resize(THUMB_LIST_SIZE, THUMB_LIST_SIZE, Image.INTERPOLATE_LANCZOS)
+			var thumb_small: ImageTexture = item["thumb"]["small"]
+			thumb_small.update(image)
+
+		semaphore.post()
 
 	while _thread_work:
 		if _thread_queue.is_empty():
@@ -1011,47 +1035,22 @@ func _thread_process() -> void:
 			semaphore.wait()
 
 			if instance is Node2D:
-				_camera_2d.set_enabled(true)
-				_camera_3d.set_current(false)
-
-				_focus_camera_on_node_2d(instance)
+				_camera_3d.call_deferred(&"set_current", false)
+				_camera_2d.call_deferred(&"set_enabled", true)
+				call_deferred(&"_focus_camera_on_node_2d", instance)
 			else:
-				_camera_2d.set_enabled(false)
-				_camera_3d.set_current(true)
+				_camera_2d.call_deferred(&"set_enabled", false)
+				_camera_3d.call_deferred(&"set_current", true)
+				call_deferred(&"_focus_camera_on_node_3d", instance)
 
-				_focus_camera_on_node_3d(instance)
-
-			RenderingServer.frame_pre_draw.connect(preview_frame_started, Object.CONNECT_DEFERRED | Object.CONNECT_ONE_SHOT)
+			call_deferred(&"connect", &"thumb_created", update_thumb.bind(item), Object.CONNECT_ONE_SHOT)
+			call_deferred(&"_create_thumb")
 			semaphore.wait()
 
-			var image: Image = _viewport.get_texture().get_image()
-			image.resize(THUMB_GRID_SIZE, THUMB_GRID_SIZE, Image.INTERPOLATE_LANCZOS)
-
-			var thumb: Dictionary = item["thumb"]
-
-			var thumb_large: ImageTexture = thumb["large"]
-			thumb_large.changed.connect(semaphore.post, Object.CONNECT_DEFERRED | Object.CONNECT_ONE_SHOT)
-			thumb_large.update(image)
-			semaphore.wait()
-
-			if _cache_enabled:
-				_save_thumb_to_disk(item["id"], image)
-
-			image = _viewport.get_texture().get_image()
-			image.resize(THUMB_LIST_SIZE, THUMB_LIST_SIZE, Image.INTERPOLATE_LANCZOS)
-
-			var thumb_small: ImageTexture = thumb["small"]
-			thumb_small.changed.connect(semaphore.post, Object.CONNECT_DEFERRED | Object.CONNECT_ONE_SHOT)
-			thumb_small.update(image)
-			semaphore.wait()
-
-			instance.tree_exited.connect(semaphore.post)
+			instance.call_deferred(&"connect", &"tree_exited", semaphore.post)
 			instance.queue_free()
 			semaphore.wait()
 
-			# Otherwise it crashes sometimes.
-			RenderingServer.frame_pre_draw.connect(semaphore.post, Object.CONNECT_DEFERRED | Object.CONNECT_ONE_SHOT)
-			semaphore.wait()
 
 func _queue_update_thumbnail(id: int) -> void:
 	if _thumbnails.has(id):
